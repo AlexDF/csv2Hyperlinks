f960821619f3adbe74f11852d6b3eeba3403f422
Alexandre DeFreitas <abd9@njit.edu>
2014-10-01 21:30:59 -0400

    Can load csv data and print each school name on its own line

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_startup_erros',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
+$row_num = 0;
+
+ini_set('auto_detect_line_endings', TRUE);
+if( ($handle = fopen("csv/hd2013.csv", "r")) !== FALSE ) {
+  while( ($row = fgetcsv($handle, 1000, ",")) != FALSE) {
+    if( $row_num == 0 ) {
+      $column_heading = $row;
+      $row_num = 1;
+    } else {
+      $record = array_combine($column_heading, $row);
+      $records[] = $record;
+    } // end if-else
+  } // end while
+  
+  fclose( $handle );
+} // end if 
+
+foreach($records as $record) {
+  echo $record['INSTNM'] . "<br>";
+}
+
+?>
